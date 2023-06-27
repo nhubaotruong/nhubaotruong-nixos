d21472e05017cac8f26f4a2a502a9f038f7712cb
Như Bảo Trương <28810481+nhubaotruong@users.noreply.github.com>
2023-06-27 17:38:12 +0700

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -195,6 +195,7 @@ options iwlwifi power_save=1
     supergfxd.enable = true; # Supergfxd
     ddccontrol.enable = true; # DDC Control
     chrony.enable = true; # Chrony
+    power-profiles-daemon.enable = false; # Power Profiles Daemon
   };
 
   # Fonts
@@ -282,6 +283,27 @@ options iwlwifi power_save=1
     '';
   };
   systemd.services.NetworkManager-wait-online.enable = false;
+
+  # TLP
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "balance_power";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+      SCHED_POWERSAVE_ON_AC = 1;
+      SCHED_POWERSAVE_ON_BAT = 1;
+      PLATFORM_PROFILE_ON_AC = "balanced";
+      PLATFORM_PROFILE_ON_BAT = "low-power";
+      AHCI_RUNTIME_PM_ON_AC = "auto";
+      AHCI_RUNTIME_PM_ON_BAT = "auto";
+      RUNTIME_PM_ON_AC = "auto";
+      RUNTIME_PM_ON_BAT = "auto";
+    };
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
