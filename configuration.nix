47d99ce95fa0297b9ad611a3f676081e92d944d4
bao.truong <bao.truong@parcelperform.com>
2023-07-02 23:54:08 +0700

    Add gamemode env

--- configuration.nix
+++ configuration.nix
@@ -382,6 +382,8 @@
     XMODIFIERS = "@im=ibus";
     SDL_VIDEO_MINIMIZE_ON_FOCUS_LOSS = "0";
     ENVFS_RESOLVE_ALWAYS = "1";
+    GAMEMODERUNEXEC =
+      "env __NV_PRIME_RENDER_OFFLOAD=1 env __GLX_VENDOR_LIBRARY_NAME=nvidia env __VK_LAYER_NV_optimus=NVIDIA_only";
   };
 
   # Nvidia
